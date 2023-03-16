027f415a7a9b40917a144b249d473bbe8d66fb7d
Paloma Tejeda <palomatejeda81@gmail.com>
2023-03-16 22:03:31 +0100

    vuelidate

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -1,62 +1,7 @@
 <template>
   <main class="contact">
     <CalendlyComponent />
-    <div class="contact__form">
-      <h2 class="contact__title">Me contacter</h2>
-      <label class="" for="prenom">
-        <input
-          class="contact__form__prenom"
-          type="text"
-          id="prenom"
-          placeholder="Votre prénom"
-          name="prenom"
-        />
-      </label>
-
-      <label class="" for="nom">
-        <input
-          type="text"
-          class="contact__form__nom"
-          id="nom"
-          placeholder="Votre nom"
-          name="nom"
-        />
-      </label>
-      <label class="" for="telephone">
-        <input
-          class="contact__form__telephone"
-          type="text"
-          id="telephone"
-          placeholder="Votre numéro de téléphone"
-          name="telephone"
-        />
-      </label>
-
-      <label class="" for="email">
-        <input
-          class="contact__form__email"
-          type="email"
-          id="email"
-          placeholder="Votre email"
-          name="mail"
-        />
-      </label>
-
-      <label class="" for="message">
-        <textarea
-          class="contact__form__message"
-          id="message"
-          placeholder="Votre message"
-          name="message"
-        ></textarea>
-      </label>
-    </div>
-    <div class="contact__info">
-      <div class="contact__info__coordonnees">
-        <p>730 Route de St Julien, 74520 Valleiry</p>
-        <p>Tél : 06 13 10 17 45</p>
-      </div>
-    </div>
+    <FormComponent />
 
     <iframe
       class="map"
@@ -72,45 +17,12 @@
   display: flex;
   flex-direction: column;
   gap: 2rem;
-  &__form {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 0.5rem;
-  }
 
-  &__info {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  .map {
+    width: 100%;
+    height: 300px;
+    border: 0;
+    padding: 0 1rem;
   }
-  &__title {
-    text-align: center;
-    font-family: "Dancing Script", cursive;
-  }
-
-  &__prenom {
-  }
-
-  &__nom {
-  }
-
-  &__telephone {
-  }
-  &__email {
-  }
-
-  &__prestation {
-  }
-
-  &__coordonnees {
-  }
-}
-.map {
-  width: 100%;
-  height: 300px;
-  border: 0;
-  padding: 0 1rem;
 }
 </style>
